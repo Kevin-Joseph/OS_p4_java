f8a67b0a16b6c0c41cf3be8f09d3dbffd18939ff
Kevin Kulda <KevinJKulda@gmail.com>
2020-04-10 13:49:31 -0500

    just before making it multi-threaded

--- src/prog4/MMIO_Reader.java
+++ src/prog4/MMIO_Reader.java
@@ -18,164 +18,95 @@ import java.nio.ByteOrder;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.security.SecureRandom;
+import java.util.concurrent.Semaphore;
 
 public class MMIO_Reader {
 	
 	static class Reader{
-		private RandomAccessFile raFile;
 		private MappedByteBuffer mbb;
 		private int numNodes;
 		
 		
 		void initReader(MappedByteBuffer mbb, long size ) {
-			/*
-			try {
-				this.raFile = new RandomAccessFile(file, "r");
-			} catch (FileNotFoundException e) {
-				System.out.println("Could not find file specified");
-				e.printStackTrace();
-			}
-			
-			long size = 0;
-			try {
-				size = raFile.length();
-				System.out.println("size of file: "+ size);
-			} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			
-			try {
-				this.mbb = this.raFile.getChannel().map(FileChannel.MapMode.READ_ONLY, 0, size);
-				mbb.order(ByteOrder.LITTLE_ENDIAN);
-				mbb.load();
-			} catch (IOException e) {
-				System.out.println("Could not map file for MappedByteBuffer.");
-				e.printStackTrace();
-			}
-			*/
 			this.mbb = mbb;
-
 			this.numNodes = (int) (size/16);
-			System.out.println("numNodes: "+ numNodes);
+			//System.out.println("numNodes: "+ numNodes);
 		}
 		
 		void readNode() {
 			SecureRandom sr = new SecureRandom();
 			
 			int sr1 = Math.abs((sr.nextInt() % (numNodes)));
-			//sr1 = Math.abs(sr1);
-			//mbb.order(ByteOrder.LITTLE_ENDIAN);
-			//if(!mbb.isLoaded()) {
-			//	mbb.load();                               //may not need to do this every time
-			//}
-			System.out.println();					  // there may be a check for it.
+			System.out.println("**********************************");
 			//System.out.println("Node Index: " + sr1);
 			System.out.println("Node Index: " + mbb.getInt(16*sr1));
 			System.out.println("Node value[0]: " + (char)mbb.get((16*sr1)+4));
 			System.out.println("Node value[1]: " + (char)mbb.get((16*sr1)+5));
 			System.out.println("Left Child Index: " + (int)mbb.getInt((16*sr1)+8));
 			System.out.println("Right Child Index: " + (int)mbb.getInt((16*sr1)+12));
+			System.out.println("**********************************");
 
 			int leftChild = (int)mbb.getInt((16*sr1)+8);
 			while(leftChild != -1) {
-				System.out.println();
+				System.out.println("**********************************");
 				//System.out.println(leftChild + " ");
 				System.out.println("Left Child Index: " + mbb.getInt(16*leftChild));
 				System.out.println("Node value[0]: " + (char)mbb.get((16*leftChild)+4));
 				System.out.println("Node value[1]: " + (char)mbb.get((16*leftChild)+5));
 				System.out.println("Left Child Index: " + (int)mbb.getInt((16*leftChild)+8));
 				System.out.println("Right Child Index: " + (int)mbb.getInt((16*leftChild)+12));
+				System.out.println("**********************************");
+
 				leftChild = (int)mbb.getInt((16*leftChild)+8);
 			}
 			
 			//sleep(100); add later
 		}
-		
-		/*
-		void closeReader() {
-			try {
-				this.raFile.close();
-			} catch (IOException e) {
-				System.out.println("Could not close random access file correctly");
-				e.printStackTrace();
-			}
-		}
-		*/
+
 	}
 	
 	
 	
 	
 	static class Writer{
-		private RandomAccessFile raFile;
 		private MappedByteBuffer mbb;
 		private int numNodes;
 
 
 		void initWriter(MappedByteBuffer mbb, long size) {
-			/*
-			try {
-				this.raFile = new RandomAccessFile(file, "w");
-			} catch (FileNotFoundException e) {
-				System.out.println("Could not find file specified");
-				e.printStackTrace();
-			}
-			
-			
-			long size = 0;
-			try {
-				size = raFile.length();
-				System.out.println("size of file: "+ size);
-			} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			
-			try {
-				 this.mbb = this.raFile.getChannel().map(FileChannel.MapMode.READ_WRITE, 0, size);
-			} catch (IOException e) {
-				System.out.println("Could not mape file for MappedByteBuffer.");
-				e.printStackTrace();
-			}
-			*/
 			this.mbb = mbb;
-			
 			this.numNodes = (int) (size/16);
-			System.out.println("numNodes: "+ numNodes);
+			//System.out.println("numNodes: "+ numNodes);
 			
 		}
 		
 		void writeNode() {
 			SecureRandom sr = new SecureRandom();
-			
 			int sr1 = Math.abs((sr.nextInt() % (numNodes)));
-			
-			
-			
-			
-			System.out.println();
+		    int uLimit = 90, lLimit = 65, val = 0;
+	        val = (Math.abs(sr.nextInt()) % (uLimit - lLimit + 1))+ lLimit;
+	        //System.out.println("val is: " + val);
+	        mbb.put(((16*sr1)+4),(byte)val);
+	        val = (Math.abs(sr.nextInt()) % (uLimit - lLimit + 1))+ lLimit;
+	        //System.out.println("val is: " + val);
+	        mbb.put(((16*sr1)+5),(byte)val);
+			System.out.println("**********************************");
 			//System.out.println("Node Index: " + sr1);
 			System.out.println("Node Index: " + mbb.getInt(16*sr1));
 			System.out.println("Node value[0]: " + (char)mbb.get((16*sr1)+4));
 			System.out.println("Node value[1]: " + (char)mbb.get((16*sr1)+5));
 			System.out.println("Left Child Index: " + (int)mbb.getInt((16*sr1)+8));
 			System.out.println("Right Child Index: " + (int)mbb.getInt((16*sr1)+12));
+			System.out.println("**********************************");
+
 		}
 		
-		/*
-		void closeWriter() {
-			try {
-				raFile.close();
-			} catch (IOException e) {
-				System.out.println("Could not close random access file correctly");
-				e.printStackTrace();
-			}
-		}
-		*/
+		//sleep(100);
 	}
 
-	
+	//private final Semaphore empty = new Semaphore(MAX_ALLOWED_IN_QUEUE, true); // added
+	//private final Semaphore full = new Semaphore(MAX_ALLOWED_IN_QUEUE, true); // added
+	//private final Semaphore mutex = new Semaphore(1, true); // added
 	
 	public static void main(String[] args) {
 		// Check if the correct number of command line arguments have been passed.
@@ -217,9 +148,6 @@ public class MMIO_Reader {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-
-		//int numNodes = (int) (size/16);
-		//System.out.println("numNodes: "+ numNodes);
 		
 		
 		MappedByteBuffer mbb = null;
@@ -234,47 +162,6 @@ public class MMIO_Reader {
 		}
 
 		
-		
-		
-		
-		/*
-		
-		for(int i = 0; i < size; i++) {
-			System.out.print((char)mbb.get());
-		}
-		
-		SecureRandom sr = new SecureRandom();
-		
-		int sr1 = (sr.nextInt() % (numNodes));
-		//System.out.println();
-		//for(int i = 0; i < 200; i++) {
-		//	sr1 = (sr.nextInt() % (numNodes));
-		//	sr1 = Math.abs(sr1);
-		//	System.out.print(sr1 + " ");
-		//}
-
-		mbb.order(ByteOrder.LITTLE_ENDIAN);
-		mbb.load();
-		System.out.println();
-		System.out.println(sr1 + " ");
-		System.out.println(mbb.getInt(16*sr1));
-		System.out.println((char)mbb.get((16*sr1)+4));
-		System.out.println((char)mbb.get((16*sr1)+5));
-		System.out.println((int)mbb.getInt((16*sr1)+8));
-		System.out.println((int)mbb.getInt((16*sr1)+12));
-
-		int leftChild = (int)mbb.getInt((16*sr1)+8);
-		while(leftChild != -1) {
-			System.out.println();
-			System.out.println(leftChild + " ");
-			System.out.println(mbb.getInt(16*leftChild));
-			System.out.println((char)mbb.get((16*leftChild)+4));
-			System.out.println((char)mbb.get((16*leftChild)+5));
-			System.out.println((int)mbb.getInt((16*leftChild)+8));
-			System.out.println((int)mbb.getInt((16*leftChild)+12));
-			leftChild = (int)mbb.getInt((16*leftChild)+8);
-		}
-		*/
 		Reader reader = new Reader();
 		reader.initReader(mbb,size);
 		reader.readNode();
@@ -285,10 +172,7 @@ public class MMIO_Reader {
 		writer.initWriter(mbb, size);
 		writer.writeNode();
 		
-		
-		
-		//reader.closeReader();
-	
+			
 	}
 
 }
